6d432fb466ddac4d0a626be777bde27bd0788c9c
wang xi <kikohz@gmail.com>
2017-09-23 23:08:28 +0800

    add source

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'iMileage' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for iMileage
+  pod 'Alamofire', '~> 4.5'
+
+  target 'iMileageTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'iMileageUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ iMileage/Home/HomeViewController.swift
@@ -0,0 +1,44 @@
+//
+//  HomeViewController.swift
+//  iMileage
+//
+//  Created by wang xi on 2017/9/23.
+//  Copyright © 2017年 wang xi. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.setup()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func setup() {
+        self.configUi()
+    }
+    
+    func configUi() {
+        self.navigationItem.title = "爱里程"
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
